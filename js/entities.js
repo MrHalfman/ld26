b15a468243452b68724ed9becd19a20fc80f8b73
MrHalfman <jdutriaux@gmail.com>
2013-04-27 15:57:44 +0200

    Spritewidth & height fix

--- js/entities.js
+++ js/entities.js
@@ -55,6 +55,8 @@ var MoveableItem = me.ObjectEntity.extend({
         this.collidable = true;
 
         //this.renderable = new me.AnimationSheet(0, 0, "furnitures", 16, 16);
+        settings.spritewidth = 16;
+        settings.spriteheight = 16;
         this.renderable.addAnimation("sofa", [2]);
         this.renderable.setCurrentAnimation(settings.type);
 
