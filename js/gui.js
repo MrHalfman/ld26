3746228c744c3c4af1b219866619413718be9a47
MrHalfman <jdutriaux@gmail.com>
2013-04-28 22:40:45 +0200

    Added button to mute sound

--- js/gui.js
+++ js/gui.js
@@ -14,3 +14,22 @@ var SpellButton = me.GUI_Object.extend({
         console.log("Clicked on " + this.spell + " button");
     }
 });
+
+var SoundButton = me.GUI_Object.extend({
+    init: function (x, y) {
+        settings = {};
+        settings.image = "soundmute";
+        settings.spritewidth = 32;
+        settings.spriteheight = 32;
+        this.parent(x, y, settings);
+    },
+    onClick: function () {
+        if (MutedSound) {
+            MutedSound = false;
+            me.audio.unmuteAll();
+        } else {
+            MutedSound = true;
+            me.audio.muteAll();
+        }
+    }
+})
