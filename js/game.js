0bd287d9494ff97ccbf8f7d180dad6cf954c7c6a
MrHalfman <jdutriaux@gmail.com>
2013-04-28 16:38:39 +0200

    Updated splash screen

--- js/game.js
+++ js/game.js
@@ -12,7 +12,8 @@ var game = {
         { name: "alpha1", type: "tmx", src: "datas/maps/alpha1.tmx" },
         { name: "meta", type: "image", src: "datas/tilesets/metaset2.png" },
         { name: "selected", type: "image", src: "datas/sprites/select.png" },
-        { name: "maintheme", type: "audio", src: "datas/sounds/" }
+        { name: "maintheme", type: "audio", src: "datas/sounds/" },
+        { name: "splashscreen", type: "image", src: "datas/images/title.png" }
     ],
     onload: function () {
         if (!me.video.init('screen', 800, 600, true)) {
@@ -24,6 +25,7 @@ var game = {
 
         me.audio.init("mp3");
         me.audio.enable();
+        me.audio.setVolume(0.5);
         me.loader.onload = this.loaded.bind(this);
         me.loader.preload(game.assets);
         me.state.change(me.state.LOADING);
@@ -52,8 +54,8 @@ var PlayScreen = me.ScreenObject.extend({
 var StartScreen = me.ScreenObject.extend({
     init: function () {
         this.parent(true);
-        this.logo = new me.Font('century gothic', 32, 'white');
         this.presstoplay = new me.Font('century gothic', 24, 'white');
+        this.background = me.loader.getImage("splashScreen");
         this.invalidate = false;
         me.input.bindKey(me.input.KEY.UP, "up");
         me.input.bindKey(me.input.KEY.DOWN, "down");
@@ -62,21 +64,19 @@ var StartScreen = me.ScreenObject.extend({
     },
     draw: function (context) {
         me.video.clearSurface(context, "black");
-
-        logo_width = this.logo.measureText(context, "Shrink Shift").width;
+        context.drawImage(this.background, 0, 0);
         presstoplay_width = this.logo.measureText(context, "Press enter to play.").width;
-        this.logo.draw(context,
-                        "Shrink Shift",
-                        ((me.video.getWidth() - logo_width) / 2),
-                        (me.video.getHeight()) / 2);
         this.presstoplay.draw(context,
                         "Press enter to play.",
                         ((me.video.getWidth() - presstoplay_width) / 2),
                         (me.video.getHeight() + 60) / 2);
     },
     update: function () {
-        if (me.input.isKeyPressed("use"))
+        if (me.input.isKeyPressed("use")) {
+            me.audio.stopTrack("maintheme");
+            me.audio.unload("maintheme");
             me.state.change(me.state.PLAY);
+        }
     },
     onDestroyEvent: function () {
         this.logo = null;
