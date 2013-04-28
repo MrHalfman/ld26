509290397dec122cec313700687afeb8a204dd60
Demurgos <demurgos@hotmail.fr>
2013-04-28 17:51:47 +0200

    Le perso suit les cases

--- js/entities.js
+++ js/entities.js
@@ -2,6 +2,18 @@
 var IsDummy = false, PlayerDirection = "top", selectedItem = null, selectedSprite;
 console.log("Commit 144");
 
+function getSmoothGridPos(pos) {
+    return { x: pos.x / 32, y: pos.y / 32 };
+}
+
+function getGridPos(pos) {
+    var rep = getSmoothGridPos(pos);
+    rep.x = Math.floor(rep.x + 0.5);
+    rep.y = Math.floor(rep.y + 0.5);
+    return rep;
+}
+
+
 var PlayerEntity = me.ObjectEntity.extend({
     init: function (x, y, settings) {
         this.parent(x, y, settings);
@@ -39,6 +51,20 @@ var PlayerEntity = me.ObjectEntity.extend({
             "remove": false
         };
         this.lastPositions = { x: this.pos.x, y: this.pos.y };
+        this.sGridPos = getSmoothGridPos(this.pos);
+        this.prevSGridPos = getSmoothGridPos(this.lastPositions);
+
+        this.hardPos = getGridPos(this.pos);
+        this.lastHardPos = getGridPos(this.lastPositions);
+
+        this.blocked = getGridPos(this.pos);
+        this.blocked.since = 30;
+
+        this.readyNode = true;
+        this.onNodeSince = 30;
+
+        this.absOnGrid = 0;
+
         this.collisiondInterp = 4;
     },
     changedirection: function (direction) {
@@ -72,6 +98,39 @@ var PlayerEntity = me.ObjectEntity.extend({
             this.power[power] = false;
         }
     },
+    gridMovement: function () {
+
+        this.blocked.since++;
+        var blockMove = false;
+
+        if (Math.floor(this.sGridPos.y) != Math.floor(this.prevSGridPos.y) && this.hardPos.y != this.lastHardPos.y) { //changement de case Y
+            if (this.blocked.y != this.hardPos.y) {
+                this.blocked = getGridPos(this.pos);
+                this.blocked.since = 0;
+            }
+        }
+        if (Math.floor(this.sGridPos.x) != Math.floor(this.prevSGridPos.x) && this.hardPos.x != this.lastHardPos.x) { //changement de case Y
+            if (this.blocked.x != this.hardPos.x) {
+                this.blocked = getGridPos(this.pos);
+                this.blocked.since = 0;
+            }
+        }
+
+
+        if (this.blocked.since < 10) {
+            this.pos.x = 32 * Math.floor(this.hardPos.x);
+            this.pos.y = 32 * Math.floor(this.hardPos.y);
+            this.lastPositions = { x: this.pos.x, y: this.pos.y };
+            this.vel.x = 0;
+            this.vel.y = 0;
+            this.updateMovement();
+        } else if (this.blocked.since > 200) {
+            this.blocked = getGridPos(this.pos);
+            this.blocked.since = 0;
+        }
+               
+
+    },
     update: function () {
         if (!IsDummy) {
             var Dummy = new DummySelector(this.pos.x + (this.width/2), this.pos.y + (this.height/2), { direction: PlayerDirection });
@@ -85,14 +144,24 @@ var PlayerEntity = me.ObjectEntity.extend({
         } else if (me.input.isKeyPressed('right')) {
             this.vel.x += this.accel.x * me.timer.tick;
             this.changedirection("right");
-        }
-
-        if (me.input.isKeyPressed('up')) {
+        } else if (me.input.isKeyPressed('up')) {
             this.vel.y -= this.accel.y * me.timer.tick;
             this.changedirection("top");
         } else if (me.input.isKeyPressed('down')) {
             this.vel.y += this.accel.y * me.timer.tick;
             this.changedirection("bottom");
+        }
+
+        if (this.vel.y != 0 && (PlayerDirection == "top" || PlayerDirection == "bottom")) {
+            var mod = this.pos.x % 32;
+            if (mod > 3 && mod < 29) {
+                this.vel.y = 0;
+            }
+        } else if (this.vel.x != 0 && (PlayerDirection == "left" || PlayerDirection == "right")) {
+            var mod = this.pos.y % 32;
+            if (mod > 3 && mod < 29) {
+                this.vel.x = 0;
+            }
         }
 
         this.updateMovement();
@@ -109,10 +178,23 @@ var PlayerEntity = me.ObjectEntity.extend({
 
         // Check if moved
         if (moveAllowed) {
+            this.sGridPos = getSmoothGridPos(this.pos);
+            this.prevSGridPos = getSmoothGridPos(this.lastPositions);
+            var save = { x: this.hardPos.x, y: this.hardPos.y };
+            this.hardPos = getGridPos(this.pos);
+            if (this.hardPos.y != save.y || this.hardPos.x != save.x) {
+                this.lastHardPos.x = save.x
+                this.lastHardPos.y = save.y
+            }
+            
             this.lastPositions = { x: this.pos.x, y: this.pos.y };
+            
+            this.gridMovement();
             return true;
         } else {
-            this.pos = { x: this.lastPositions.x, y: this.lastPositions.y };
+            this.pos.x = this.lastPositions.x;
+            this.pos.y = this.lastPositions.y;
+            return true;
         }
         return false;
     }
